56bd2c76adee99f7f7feda9a7bc2e1591db2cff1
frozen1000 <ghimel69@gmail.com>
2022-08-31 15:57:13 -0500

    prueba CRUD completa Program.cs

--- TreeAppGym.App.Consola/Program.cs
+++ TreeAppGym.App.Consola/Program.cs
@@ -1,7 +1,9 @@
 using System;
 using TreeAppGym.App.Dominio;
 using TreeAppGym.App.Persistencia;
-
+using System.Collections.Generic;
+//permite leer los namespace
+// Este repositorio implementa la interfaz
 namespace TreeAppGym.App.Consola
 {
     class Program
@@ -10,20 +12,59 @@ namespace TreeAppGym.App.Consola
         //Coloco un objeto que me permite llamar a cada uno de los métodos CRUD
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World Entity Framework!");
+            Console.WriteLine("Bienvenidos al Gimnasio");
             AdicionarPlanNutricional();
+            ConsultarPlanNutricional();
+            ActualizarPlanNutricional();
+            // EliminarPlanNutricional();
+
         }
         //Clase específica para crear el objeto PlanNutricional
         private static void AdicionarPlanNutricional()
         {
             var planNutricional = new PlanNutricional
             {
-                Categoria=Categoria.basica,
-                Descripcion="Alimentación saludable para principiantes",
-                Imagen="123456"
+                Categoria=Categoria.avanzada,
+                Descripcion="Alimentación saludable pro",
+                Imagen="url.dietaespectacular"
                 
             };
             _repoPlanNutricional.CrearPlanNutricional(planNutricional);
+            
+        }
+        //Clase específica para consultar el objeto PlanNutricional
+        private static void ConsultarPlanNutricional()
+        {
+            var planNutricional = new PlanNutricional
+            {
+             
+            }; 
+           _repoPlanNutricional.ConsultarPlanNutricional(2); 
+               
+        }
+        //Clase específica para eliminar el objeto PlanNutricional
+        private static void EliminarPlanNutricional()
+        {
+            var planNutricional = new PlanNutricional
+            {
+             
+            }; 
+           _repoPlanNutricional.EliminarPlanNutricional(2); 
+               
+        }
+        //Clase específica para actualizar el objeto PlanNutricional
+        private static void ActualizarPlanNutricional()
+        {
+            var planNutricional = new PlanNutricional
+            {
+                
+                Categoria=Categoria.avanzada,
+                Descripcion="Alimentación saludable pro",
+                Imagen="url.dietaespectacular"
+                 
+            }; 
+           _repoPlanNutricional.ActualizarPlanNutricional(planNutricional); 
+               
         }
 
     }
